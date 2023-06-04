f5507ea50c33f79e233a879c7b5833bdd68ff8b1
somak <somak.jha@gmail.com>
2023-06-04 23:52:53 +0530

    hamburger icon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import '../styles/stylesComp/Navbar.scss'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faXmark } from '@fortawesome/free-solid-svg-icons'
+
 
 function Navbar() {
   const [hamburger, setHamburger] = useState(false);
@@ -39,7 +38,8 @@ function Navbar() {
           </li>
 
           <li onClick={() => hamburgerMenu()}>
-          <a className='hb' href="#contact">O</a>
+          <a className='hb' href="#contact"><svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" width="24px" height="24px"><path d="M 2 5 L 2 7 L 22 7 L 22 5 L 2 5 z M 2 11 L 2 13 L 22 13 L 22 11 L 2 11 z M 2 17 L 2 19 L 22 19 L 22 17 L 2 17 z"/></svg></a>
+          
           </li>
         </ul>
       </nav>
